519a7ef1b7ac30089eb708f2f1507f81dc987353
Oliver Johnson <oliverdejohnson@gmail.com>
2014-04-10 12:55:11 +0100

    test

---
+++ Quiz47/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Quiz47
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("OOPs...Something is not right: " + e.Exception.Message, "Exception Handler", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+
+    }
+}
